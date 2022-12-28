b92000c15263c620962b3d9974d9030a491fa763
Barak Bloch <barak.bloch@gmail.com>
2022-12-28 14:55:56 +0200

    Init

---
+++ README.md
@@ -0,0 +1,26 @@
+# psh / Simple Python shell
+
+A basic Python based UNIX shell
+This is in WIP and only intended for learning purpose
+
+
+## Running
+`
+python psh.py
+- 
+
+
+## Features
+- command history view (in memory only)
+- a config file (see psh.config for example)
+- set and view env vars
+- show the current directory, implementation of cd and exit
+## TODO
+- pipe (|) support is  vary important but not implemented yet
+- compilation support
+- save history in disk (and load it in new psh)
+- colors for nicer shell view
+- plugins support
+- tests
+- support for ~  in paths
+- better handling of returned errors from commands
---
+++ psh.py
@@ -0,0 +1,104 @@
+#!/bin/env python
+
+import os, sys
+import subprocess
+import json
+
+default_envvars = {
+    "PSH_MAX_HISTORY_SIZE": 100,
+    "PSH_PROMPT" : f"$ ",
+    "TERM" : "xterm-256color" # TODO
+}
+
+def load_config():
+
+    ## default config
+    for k in default_envvars.keys():
+        os.environ[k] = str(default_envvars[k])
+
+    ### load config
+    conf_file = f"{os.environ.get('HOME')}/.psh"
+    try:
+        with open(conf_file) as f:
+            data = json.loads(f.read())
+            if "envvars" in data:
+                for v in data["envvars"]:
+                    os.environ[v] = data["envvars"][v]
+    except:
+        print("no config file found")
+
+load_config()
+history = []
+
+def cwd(absolute=False):
+    d = subprocess.check_output('pwd').decode('utf-8').replace("\n", "")
+    if absolute:
+        return d
+    return d.split("/").pop() or "/"
+
+last_dir = cwd(absolute=True)
+def history_cmd():
+    [print(i) for i in history[::-1]]
+
+def insert_history(cmd):
+    if len(history)  == os.environ.get("MAX_HISTORY_SIZE"):
+        history.pop(0)
+        history.append(cmd)
+    else:
+        history.append(cmd)
+
+def help(cmd):
+    print(f"this is psh, a simple shell written in Python by Barak Bloch")
+
+def cd(target):
+    global last_dir
+    curr_dir = cwd(absolute=True)
+    if not target:
+        print("target is missing")
+        return
+
+    if target == "-":
+        os.chdir(os.path.abspath(last_dir))
+    else:
+        try:
+
+            os.chdir(os.path.abspath(target))
+        except Exception:
+            print(f"cd: no such file or directory: {target}")
+    last_dir = curr_dir
+
+def run_command(cmd):
+    try:
+        subprocess.run(cmd.split())
+    except Exception as e:
+        print(f"psh: command not found: {cmd}")
+
+def env_cmd(var=None):
+    if var:
+        v = var.split("=")
+        if len(v) == 1:
+            print(os.environ.get(var,f"env variable {var} not set"))
+        else:
+            os.environ[v[0]] = v[1]
+    else:
+        print(dict(os.environ))
+
+if __name__ == "__main__":
+    while True:
+        inp = input(f"{cwd()} {os.environ.get('PSH_PROMPT')}").strip()
+
+        if inp.startswith("exit"):
+            sys.exit(0)
+        elif inp.startswith("help"):
+            help(inp)
+        elif inp.startswith("cwd"):
+            print(cwd(absolute=True))
+        elif inp.startswith("cd"):
+            cd(inp[3:].strip())
+        elif inp.startswith("history"):
+            history_cmd()
+        elif inp.startswith("env"):
+            env_cmd(inp[3:].strip())
+        else:
+            run_command(inp)
+        insert_history(inp)
